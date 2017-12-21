7fec0aa0566e1ff536433d3352a1d2875c4f4cdf
LucasMAgnana <lucas.magnana@gmail.com>
2017-12-21 17:44:33 +0100

    fonction pseudo-équivalence finie

--- src/Equivalence.cpp
+++ src/Equivalence.cpp
@@ -22,18 +22,17 @@ std::string Automate2ExpressionRationnelle(sAutoNDE at){
 
 bool PseudoEquivalent(const sAutoNDE& a1, const sAutoNDE& a2, unsigned int word_size_max, string word) {
   //TODO d√©finir cette fonction
-	cout << word << "  " << word.size() << endl;
-	if((Accept(a1, word) && Accept(a2, word)) || (!Accept(a1, word) && !Accept(a2, word))){
-		if(word.size() == word_size_max) 
+	if((Accept(a1, word) && Accept(a2, word)) || (!Accept(a1, word) && !Accept(a2, word))){ //si le mot est acceptÈ ou refusÈ par les deux automates
+		if(word.size() == word_size_max) // on stoppe la recursivitÈ si le mot est aussi long que la taille max
 			return true;
-		else{
-			for(unsigned int symb=ASCII_A; symb<ASCII_Z; symb++){
+		else{ //sinon
+			for(unsigned int symb=ASCII_A; symb<ASCII_Z; symb++){ //pour chaque symbole de l'alphabet
 				string test_word = word;
 				string letter;
 				letter = (char)symb;
 				test_word = test_word+letter;
-				 if (!PseudoEquivalent(a1, a2, word_size_max, test_word))
-					 return false;
+				 if (!PseudoEquivalent(a1, a2, word_size_max, test_word)) //on rÈappelle la fonction avec le mot entrÈ en paramËtre + le symbole 
+					 return false; //si on se retrouve avec un false, on retourne false en cascade
 			}
 			return true;
 		}
